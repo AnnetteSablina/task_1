f12d2116cb53e0d6bf863e3c3693289cfac4fbf6
Annette Sablina <oiveshka@gmail.com>
2021-04-04 14:22:11 +0300

    Refactoring code

--- src/com/annette/task10/Section.java
+++ src/com/annette/task10/Section.java
@@ -5,6 +5,8 @@ import com.annette.utility.Mode;
 import com.annette.utility.NegativeException;
 import com.annette.utility.ZeroException;
 
+import java.util.Objects;
+
 public class Section {
     private int sectionStart;
     private int sectionEnd;
@@ -16,6 +18,41 @@ public class Section {
         this.sectionEnd = sectionEnd;
         this.divisionValue = divisionValue;
     }
+
+    public void setSectionStart(int sectionStart) {
+        this.sectionStart = sectionStart;
+    }
+
+    public void setSectionEnd(int sectionEnd) {
+        this.sectionEnd = sectionEnd;
+    }
+
+    public void setDivisionValue(int divisionValue) {
+        this.divisionValue = divisionValue;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Section section = (Section) o;
+        return sectionStart == section.sectionStart && sectionEnd == section.sectionEnd && divisionValue == section.divisionValue;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(sectionStart, sectionEnd, divisionValue);
+    }
+
+    @Override
+    public String toString() {
+        return "Section{" +
+                "[" + sectionStart +
+                ";" + sectionEnd +
+                "] with division value =" + divisionValue +
+                '}';
+    }
+
     public static Section fillSectionFields(){
         int sectionStart;
         int sectionEnd;
@@ -35,7 +72,6 @@ public class Section {
     }
 
     private static void checkSection(int sectionStart,int sectionEnd) throws SectionException {
-        System.out.println("lol");
         if (sectionStart > sectionEnd) throw new SectionException("End value is bigger then start");
     }
     public int getSectionStart() {
